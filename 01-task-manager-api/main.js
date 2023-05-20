cdfa5154b755fd40ae021d4ba5ec3387fda7d0cd
duexmach <themllearner@gmail.com>
2023-05-20 16:43:01 +0545

    working version

--- 01-task-manager-api/main.js
+++ 01-task-manager-api/main.js
@@ -1,3 +1,38 @@
-// Objectives
-// 1. Connect to cloud database
-// 2. Perform CRUD operations
+require("dotenv").config(); // importing automatically executes function
+const PORT = 3000;
+
+const express = require("express");
+app = express();
+// importing routes
+const tasks = require("./routes/tasks");
+
+// requests
+// app.get('/api/v1/tasks') -> get all tasks
+// app.post('/api/v1/tasks') -> create a new task
+// app.get('/api/v1/tasks/:id') -> get single task
+// app.patch('/api/v1/tasks/:id') -> update a task
+// app.delete('/api/v1/tasks/:id') -> delete a task
+
+// database
+const connectDB = require("./db/connect");
+
+// middleware
+app.use(express.static("./public")); // setting public directory
+app.use(express.json()); // for accessing req.body
+
+// api routes
+app.use("/api/v1/tasks", tasks);
+
+// start server iff database connection is successful
+const start = async () => {
+  try {
+    await connectDB(process.env.mongo_uri);
+    app.listen(PORT, () => {
+      console.log(`App running at port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
