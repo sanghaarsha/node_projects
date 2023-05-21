4dbab7abde4dd8e162f3ac3221e75601a6a4afdb
duexmach <themllearner@gmail.com>
2023-05-21 17:09:34 +0545

    implement async wrapper

--- 03-task-manager-self-implement/controller/tasks.js
+++ 03-task-manager-self-implement/controller/tasks.js
@@ -1,59 +1,40 @@
 const Task = require("../model/Task");
+const asyncWrapper = require("../async/asyncWrapper");
 
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
 
-const createTask = async (req, res) => {
+const createTask = asyncWrapper(async (req, res) => {
   // req.body = body data i.e. from submit form actions, etc.
   // req.params.id = from url
   newTask = req.body;
-  try {
-    data = await Task.create(newTask);
-    res.status(200).json({ data });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-};
+  data = await Task.create(newTask);
+  res.status(200).json({ data });
+});
 
-const getSingleTask = async (req, res) => {
+const getSingleTask = asyncWrapper(async (req, res) => {
   const taskID = req.params.id;
-  try {
-    const task = await Task.findOne({ _id: taskID });
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json(error);
-  }
-};
+  const task = await Task.findOne({ _id: taskID });
+  res.status(200).json({ task });
+});
 
-const editTask = async (req, res) => {
+const editTask = asyncWrapper(async (req, res) => {
   const taskID = req.params.id;
   const newData = req.body;
-  try {
-    const data = await Task.findOneAndUpdate({ _id: taskID }, newData, {
-      new: true,
-      runValidators: true,
-    }); // while updating by default, validators are not applied and returned data is old one
-    res.status(200).json({ data });
-  } catch (error) {
-    res.status(500).json(error);
-  }
-};
+  const data = await Task.findOneAndUpdate({ _id: taskID }, newData, {
+    new: true,
+    runValidators: true,
+  }); // while updating by default, validators are not applied and returned data is old one
+  res.status(200).json({ data });
+});
 
-const deleteTask = async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res) => {
   const taskID = req.params.id;
-  try {
-    const data = await Task.findOneAndDelete({ _id: taskID });
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-};
+  const data = await Task.findOneAndDelete({ _id: taskID });
+  res.status(200).json(data);
+});
 
 module.exports = {
   getAllTasks,
