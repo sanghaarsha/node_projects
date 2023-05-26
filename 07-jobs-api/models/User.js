1533fef8c0caf9b3024516cc69e94338ce8b7fd1
duexmach <themllearner@gmail.com>
2023-05-26 15:55:41 +0545

    removed: 'next', no more required to function

--- 07-jobs-api/models/User.js
+++ 07-jobs-api/models/User.js
@@ -26,10 +26,9 @@ const UserSchema = mongoose.Schema({
 });
 
 // hashing password before saving the document
-UserSchema.pre("save", async function (next) {
+UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
