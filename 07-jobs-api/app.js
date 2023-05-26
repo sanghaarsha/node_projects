a7dbfb503395dceffd96ef03d594b5b0393e3a10
duexmach <themllearner@gmail.com>
2023-05-27 00:48:51 +0545

    setup: swagger ui

--- 07-jobs-api/app.js
+++ 07-jobs-api/app.js
@@ -11,6 +11,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const app = express();
 
 const authRouter = require("./routes/auth");
@@ -39,6 +44,9 @@ app.use(xss());
 app.use(express.static("./public"));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authMiddleware, jobsRouter);
+
+// setup and server swagger doc
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // ! always use these middlewares after routes
 app.use(errorHandler);
