b2aa864a0464c05dc01024f8bf9e67ecb6b04bdb
duexmach <themllearner@gmail.com>
2023-05-26 22:36:46 +0545

    handle duplicate values

--- 07-jobs-api/middlewares/error-handler.js
+++ 07-jobs-api/middlewares/error-handler.js
@@ -1,11 +1,25 @@
 const CustomAPIError = require("../errors/custom-error");
+const { StatusCodes } = require("http-status-codes");
 
 const errorHandler = (err, req, res, next) => {
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "something went wrong try again later",
+  };
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
 
-  return res.status(500).json(err);
+  // duplicate error code : 11000
+  if (err.code && err.code == "11000") {
+    customError.msg = `this email: ${err.keyValue.email}, already in use`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    return res.status(customError.statusCode).json({ msg: customError.msg });
+  }
+
+  return res.json(err);
 };
 
 module.exports = errorHandler;
