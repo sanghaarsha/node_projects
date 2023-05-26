f6e26d8693be251756fe56c63d786352a692774e
duexmach <themllearner@gmail.com>
2023-05-27 00:41:08 +0545

    always use safety :)

--- 07-jobs-api/app.js
+++ 07-jobs-api/app.js
@@ -5,6 +5,12 @@ const MONGO_URI = process.env.MONGO_URI;
 require("express-async-errors");
 
 const express = require("express");
+// security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const app = express();
 
 const authRouter = require("./routes/auth");
@@ -16,6 +22,18 @@ const authMiddleware = require("./middlewares/auth");
 
 // middleware to parse body
 app.use(express.json());
+
+// security middlewares
+app.set("trust proxy", 1); // support reverse proxy
+app.use(
+  rateLimiter({
+    windowMs: 15 * 16 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use(express.static("./public"));
