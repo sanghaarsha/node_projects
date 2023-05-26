09839695e1046e21b06d9f07ea4ca876d0704bd4
duexmach <themllearner@gmail.com>
2023-05-26 15:07:27 +0545

    User model

---
+++ 07-jobs-api/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide name"],
+    minLength: [5, "name must be of at least 5 characters"],
+    maxLength: [50, "name cannot be of more than 50 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      ,
+      "please provide valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "please provide email"],
+    minLength: [10, "password must be of at least 10 characters"],
+    maxLength: [20, "password cannot be of more than 20 characters"],
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
